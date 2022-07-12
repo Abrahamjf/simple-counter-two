1c7e50978aa03adda6da0caab0e0997363a65ef4
Abrahamjf <abrahamjferrer@gmail.com>
2022-07-12 04:58:28 +0000

    [ADD] Se agregaron dos cifras

--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -8,10 +8,12 @@ export const Counter= (props) =>{
         <div className ="counter">
             <p>0</p>
         </div>
-        <div className="four">{props.digitOne}</div>
-        <div className="three">{props.digitTwo}</div>
-        <div className="two">{props.digitThree}</div>
-        <div className="one">{props.digitFour}</div>
+        <div className="one">{props.digitOne}</div>
+        <div className="two">{props.digitTwo}</div>
+        <div className="three">{props.digitThree}</div>
+        <div className="four">{props.digitFour}</div>
+        <div className="five">{props.digitFive}</div>
+        <div className="six">{props.digitSix}</div>
     </div>
     );
 }
@@ -32,20 +34,24 @@ export const Counter= (props) =>{
 //  }
 
  Counter.propTypes = {
-     digitFour: PropTypes.number,
-     digitThree:PropTypes.number,
-     digitThree:PropTypes.number,
-     digitOne:PropTypes.number
+    digitSix:PropTypes.number,
+    digitFive:PropTypes.number,
+    digitFour:PropTypes.number,
+    digitThree:PropTypes.number,
+    digitThree:PropTypes.number,
+    digitOne:PropTypes.number
  };
  
  let count = 0;
  setInterval(function(){
-     const one = Math.floor((count/1000)%10);
-     const two =Math.floor((count/100)%10);
-     const three = Math.floor((count/10)%10);
-     const four =Math.floor((count/1)%10);
-     console.log(one,two, three, four)
+     const one = Math.floor((count/100000)%10);
+     const two =Math.floor((count/10000)%10);
+     const three = Math.floor((count/1000)%10);
+     const four =Math.floor((count/100)%10);
+     const five = Math.floor((count/10)%10);
+     const six = Math.floor((count/1)%10);
+     console.log(one,two, three, four,five, six)
      count++;
      
-     ReactDOM.render(<Counter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, document.querySelector("#app"));
+     ReactDOM.render(<Counter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />, document.querySelector("#app"));
  },1000);
